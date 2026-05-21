4e6b13ef327371bc4e8242916f76ce88e1316d86
prit-zalavadiya-78 <pritzalavadiya0@gmail.com>
2026-05-21 16:14:57 +0530

    Update cookie settings

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -19,9 +19,9 @@ const googleAuth = async (req, res) => {
         let token = await getToken(user._id);
 
         res.cookie("token", token, {
-            httpOnly: true,
-            // secure: true,
-            sameSite: "strict",
+            httpOnly: false,
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -36,9 +36,9 @@ const googleAuth = async (req, res) => {
 const logoutUser = async (req, res) => {
     try {
         res.clearCookie("token", {
-            httpOnly: true,
-            // secure: true,
-            sameSite: "strict",
+            httpOnly: false,
+            secure: true,
+            sameSite: "none",
         });
         return res.status(200).json({message: "User logged out successfully"});
     } catch (error) {
