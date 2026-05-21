1505acae72809b313effb4e17f4a99d8dffb89da
prit-zalavadiya-78 <pritzalavadiya0@gmail.com>
2026-05-21 17:58:33 +0530

    Fix CORS origin configuration to support multiple domains

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://devoraui-1.onrender.com/",
+    origin: ["https://devoraui-1.onrender.com", "http://localhost:5173" ],
     credentials: true,
 }));
 
